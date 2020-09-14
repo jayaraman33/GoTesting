83232292ec6458dac00f3f19a56635d74db7ee58
jayaraman33 <70377145+jayaraman33@users.noreply.github.com>
2020-09-14 12:00:54 +0530

    math gotest

---
+++ sub.go
@@ -0,0 +1,7 @@
+package math
+
+// Calculate returns x + 2.
+func Subtract(x int) (result int) {
+	result = x - 2
+	return result
+}
---
+++ sub_test.go
@@ -0,0 +1,12 @@
+package math
+
+import "testing"
+
+func TestSubtract(t *testing.T) {
+
+	t.Error("Expected x-2 ")
+
+	// func add(x int)  {
+
+	// }
+}
